3aa99da7cf59a9caf59e0d24c58e1ec70a147746
Joaquin L. Pereyra <joaquinlp@infobytesec.com>
2016-08-06 17:42:18 -0300

    made it work

--- wall-randomizer.sh
+++ wall-randomizer.sh
@@ -1,4 +1,4 @@
-wallpaper=$(ls "$HOME/.dotfiles/wallpapers" | shuf -n 1)
-wallpaper_path="$HOME/.dotfiles/wallpapers/$wallpaper"
+wallpaper=$(ls "/home/joaquin/.dotfiles/wallpapers" | shuf -n 1)
+wallpaper_path="/home/joaquin/.dotfiles/wallpapers/$wallpaper"
 feh --bg-scale $wallpaper_path
 
